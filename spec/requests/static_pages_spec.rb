b0aac9a8cfe50f7f028e0ae221682d5c21ef63f6
MayankGaur <mayankgaur2307@gmail.com>
2014-06-07 16:30:14 +0530

    commiting at 4 _30

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,10 +4,25 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+  it "should have the content 'Sample App'" do
+    visit '/static_pages/home'
+    expect(page).to have_content('Sample App')
+  end
+end
+describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
     end
   end
+
+  describe "About Page" do
+  	it "should have the content 'about'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('about')
+  	end
+  end
+
 end
 
