2b710cf242f6e7c801bbe4b2b36692edd66d7103
MayankGaur <mayankgaur2307@gmail.com>
2014-06-08 02:09:29 +0530

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,20 +8,36 @@ describe "Static pages" do
     visit '/static_pages/home'
     expect(page).to have_content('Sample App')
   end
+
+  it "should have the title 'Home'" do
+    visit '/static_pages/home'
+    expect(page).to have_title("Ruby on rails|Home")
 end
+end
+
 describe "Help page" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
-    end
-  end
+   end
+   it "should have title 'help'" do
+    visit '/static_pages/help'
+    expect(page).to have_title("Ruby on rails|Help")
+  end  
+
+end
 
   describe "About Page" do
   	it "should have the content 'about'" do
   		visit '/static_pages/about'
   		expect(page).to have_content('about')
   	end
+
+    it "should have the title 'about'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on rails|About")
+    end 
   end
 
 end
